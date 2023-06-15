1cf9677cd09c59e87e8bb229d5b002a762a5ad1a
ajith82 <ajith@sketchbrahma.com>
2023-06-15 13:14:45 +0530

    scrolling is working, dynamic height is pending

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const slider = document.getElementsByClassName("slider")[0];
-slider.addEventListener("scroll",() => {
-    console.log("aaaaaaaaaaaaaa");
-})
+const box = document.getElementsByClassName("box");
+document.addEventListener("scroll", () => {
+  const startY = 200;
+  const endY = 500;
+
+  const scrollPos = window.pageYOffset;
+  if (window.pageYOffset / 2 > 485) {
+    if (scrollPos >= window.pageYOffset / 2) {
+    //   if (scrollPos < 2367) {
+        const progress = (scrollPos - startY) / (endY - startY);
+        const minHeight = 500;
+        const maxHeight = 1000;
+        const targetHeight = minHeight + progress * (maxHeight - minHeight);
+        // slider.style.height = `${targetHeight}px`;
+        slider.style.height = `2000px`;
+        for (let i = 0; i < box.length; i++) {
+          box[i].style.transform = `translate3d(-${scrollPos}px, 0px, 0px)`;
+        }
+    //   }
+    }
+  }
+});
