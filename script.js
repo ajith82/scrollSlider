1b3cc508d1476ad83f59e3e4a1e0fe17fc6016ad
ajith82 <ajith@sketchbrahma.com>
2023-06-15 15:47:10 +0530

    unwanted codes removed

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const sliderHead = document.querySelector(".sliderHead");
 const boxes = document.querySelector(".boxes");
 const box = document.getElementsByClassName("box");
 document.addEventListener("scroll", () => {
-  const startY = 200;
-  const endY = 500;
   const scrollPos = window.pageYOffset - banner.clientHeight;
   if (window.pageYOffset / 2 > 485) {
     console.log(sliderHead.clientHeight);
@@ -13,8 +11,7 @@ document.addEventListener("scroll", () => {
     const boxesHeight = boxes.clientHeight;
     const headingWidth = sliderHead.clientHeight;
     const headingHeight = sliderHead.clientWidth;
-    const progress = (scrollPos - startY) / (endY - startY);
-    slider.style.height = `${boxesWidth + boxesHeight + headingWidth + headingHeight - 600}px`;
+    slider.style.height = `${boxesWidth + boxesHeight + headingWidth + headingHeight - 1100}px`;
     for (let i = 0; i < box.length; i++) {
       box[i].style.transform = `translate3d(-${scrollPos}px, 0px, 0px)`;
     }
