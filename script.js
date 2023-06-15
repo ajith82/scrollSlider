09f0a54520fb65347078c93636f78119086a7094
ajith82 <ajith@sketchbrahma.com>
2023-06-15 15:19:23 +0530

    dynamic height added

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
+const banner = document.querySelector(".banner");
 const slider = document.getElementsByClassName("slider")[0];
+const sliderHead = document.querySelector(".sliderHead");
+const boxes = document.querySelector(".boxes");
 const box = document.getElementsByClassName("box");
 document.addEventListener("scroll", () => {
   const startY = 200;
   const endY = 500;
-
-  const scrollPos = window.pageYOffset;
+  const scrollPos = window.pageYOffset - banner.clientHeight;
   if (window.pageYOffset / 2 > 485) {
-    if (scrollPos >= window.pageYOffset / 2) {
-    //   if (scrollPos < 2367) {
-        const progress = (scrollPos - startY) / (endY - startY);
-        const minHeight = 500;
-        const maxHeight = 1000;
-        const targetHeight = minHeight + progress * (maxHeight - minHeight);
-        // slider.style.height = `${targetHeight}px`;
-        slider.style.height = `2000px`;
-        for (let i = 0; i < box.length; i++) {
-          box[i].style.transform = `translate3d(-${scrollPos}px, 0px, 0px)`;
-        }
-    //   }
+    console.log(sliderHead.clientHeight);
+    const boxesWidth = boxes.clientWidth;
+    const boxesHeight = boxes.clientHeight;
+    const headingWidth = sliderHead.clientHeight;
+    const headingHeight = sliderHead.clientWidth;
+    const progress = (scrollPos - startY) / (endY - startY);
+    slider.style.height = `${boxesWidth + boxesHeight + headingWidth + headingHeight - 600}px`;
+    for (let i = 0; i < box.length; i++) {
+      box[i].style.transform = `translate3d(-${scrollPos}px, 0px, 0px)`;
+    }
+  } else if (window.pageYOffset / 2 < 485) {
+    for (let i = 0; i < box.length; i++) {
+      box[i].style.transform = `translate3d(0px, 0px, 0px)`;
     }
   }
 });
